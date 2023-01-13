5097e05ab64d989e1c5d1b472e1dca3063d541eb
Litttle-panda <117175961+Litttle-panda@users.noreply.github.com>
2023-01-13 17:31:58 +0000

    added four questions from demo text

--- assets/JS/questions.js
+++ assets/JS/questions.js
@@ -2,12 +2,24 @@
 
 var questions = [
     {
-        questionText: "Q1. Commonly used data types do NOT include: ",
+        questionText: "Q1. Commonly used data types DO NOT include: ",
         choices: ["strings", "booleans", "alerts", "numbers"],
         answer: "alerts"
     },
     {
-        
+        questionText: "Q2. The condition in an if/else statement is enclosed within: ",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "curly brackets"   
+    },
+    {
+        questionText: "Q3. Arrays in Javascript can be used to store: ",
+        choices: ["numbers and strings", "other strings", "booleans", "all of the above"],
+        answer: "all of the above"
+    },
+    {
+        questionText: "Q4. String Values must be enclosed within _______ when being assigned to variables: ",
+        choices: ["commas", "curly brackets", "quotation marks", "parentheses"],
+        answer: "quotation marks"
     }
 ]
 //question[currentQuestionsIndex].questionText
